a5e80b4433485dfea4139af7bc85a645d07158dc
wangmingqiang <wangmingqiang@kuaishou.com>
2020-05-23 20:25:35 +0800

    t

--- src/main/java/com/example/infra/StreamDemo.java
+++ src/main/java/com/example/infra/StreamDemo.java
@@ -4,5 +4,12 @@ package com.example.infra;
  * @author wangmingqiang <wangmingqiang@kuaishou.com>
  * Created on 2020-05-23
  */
-public class StreamDeom {
+public class StreamDemo {
+
+    public static void main(String[] args) {
+        //Git is a distributed version control system.
+        //Git is free software distributed under the GPL.
+        //Git has a mutable index called stage.
+        // Git tracks changes.
+    }
 }
