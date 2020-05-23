93f6faae9b6ca72cc479cd3c589f0d48c6d722dc
wangmingqiang <wangmingqiang@kuaishou.com>
2020-05-23 20:29:07 +0800

    dsds

--- src/main/java/com/example/infra/StreamDemo.java
+++ src/main/java/com/example/infra/StreamDemo.java
@@ -11,5 +11,6 @@ public class StreamDemo {
         //Git is free software distributed under the GPL.
         //Git has a mutable index called stage.
         // Git tracks changes.
+        // Git tracks changes new.
     }
 }
